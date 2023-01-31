71a729354e28aeb79dcd15ab65efc79b366e3fff
joynal_alam <joynal.alam@enosisbd.com>
2023-01-31 17:38:56 +0600

    Implement sql data access functionalities.

---
+++ DataAccess/Data/IUserData.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public interface IUserData
+    {
+        Task<IEnumerable<UserModel>> GetUsers { get; }
+
+        Task DeleteUser(int id);
+        Task<UserModel> GetUser(int id);
+        Task InsertUser(UserModel user);
+        Task UpdateUser(UserModel user);
+    }
+}
