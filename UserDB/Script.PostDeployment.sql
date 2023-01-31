f4e1b157e9077b97721c6b8a850c27ed07c24fd7
joynal_alam <joynal.alam@enosisbd.com>
2023-01-31 16:21:21 +0600

    create database and stored procedures.

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,6 @@
+﻿--t sql code
+if not exists (select 1 from dbo.[User])
+begin
+    insert into dbo.[User] (FirstName, LastName)
+    values ('Joynal', 'Alam'), ('Sania', 'Amin'), ('John', 'Smith');
+end
